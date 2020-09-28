bf889340e70fa1cc36f179484855de39325c188c
Gareth-Walters <gareth.a.walters@gmail.com>
2020-09-28 16:28:54 +0100

    Implemented updating recipe servings

--- src/js/index.js
+++ src/js/index.js
@@ -97,3 +97,18 @@ const controlRecipe = async () => {
 };
 
 ['hashchange', 'load'].forEach((event) => window.addEventListener(event, controlRecipe));
+
+elements.recipe.addEventListener('click', (e) => {
+  if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+    // decrease button is clicked
+    if (state.recipe.servings > 1) {
+      state.recipe.updateServings('dec');
+      recipeView.updateServingsIngredients(state.recipe);
+    }
+  } else if (e.target.matches('.btn-increase, .btn-increase *')) {
+    // increase button is clicked
+    state.recipe.updateServings('inc');
+    recipeView.updateServingsIngredients(state.recipe);
+  }
+  console.log(state.recipe);
+});
